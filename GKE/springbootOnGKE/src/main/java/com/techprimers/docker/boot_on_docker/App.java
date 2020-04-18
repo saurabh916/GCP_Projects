91f9c945d33402a13b6482fe1d5fb0f454d169b7
saurabh916 <saurabh916@gmail.com>
2020-04-18 17:55:39 +0530

    Spring boot application on GKE

---
+++ GKE/springbootOnGKE/src/main/java/com/techprimers/docker/boot_on_docker/App.java
@@ -0,0 +1,30 @@
+package com.techprimers.docker.boot_on_docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+@RestController
+@EnableSwagger2 
+public class App 
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(App.class, args);
+    }
+    
+    @RequestMapping(value="/gketest")
+    public String testApp(){
+    	return "Hi there, this is for GKE";
+    }
+    
+    
+}
